c10218dc3ee76c862a0fc09ca0744d401f59ff2c
Fabian Werling <Werling.Fabian@gmail.com>
2022-12-10 16:56:25 +0100

    forcing dark mode

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,21 +4,23 @@ import Navbar from "../components/component.navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className="bg-svg flex flex-col bg-zinc-900"
-      // style={{ backgroundImage: "/background.svg" }}
-    >
-      <Head>
-        <title>Forza Horizon Time Attack</title>
-        <meta
-          name="description"
-          content="Forza Horizon Time Attack is an online leaderboard for all tracks in Forza Horizon 5. Share your best times with the community."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <main className="min-h-screen pt-10">{children}</main>
-      <Footer />
+    <div className="dark">
+      <div
+        className="flex flex-col bg-zinc-900"
+        // style={{ backgroundImage: "/background.svg" }}
+      >
+        <Head>
+          <title>Forza Horizon Time Attack</title>
+          <meta
+            name="description"
+            content="Forza Horizon Time Attack is an online leaderboard for all tracks in Forza Horizon 5. Share your best times with the community."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+        <main className="min-h-screen pt-10">{children}</main>
+        <Footer />
+      </div>
     </div>
   );
 }
