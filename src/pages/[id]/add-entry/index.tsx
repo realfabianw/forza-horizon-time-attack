9448b684d6d264ba87f12ad7bee96aa05cceaf86
Fabian Werling <Werling.Fabian@gmail.com>
2023-02-19 20:56:11 +0100

    finalise image upload workflow

--- src/pages/[id]/add-entry/index.tsx
+++ src/pages/[id]/add-entry/index.tsx
@@ -1,6 +1,6 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import { HashLoader } from "react-spinners";
 import { EntryCreateOneSchema } from "../../../../prisma/generated/schemas/createOneEntry.schema";
@@ -14,39 +14,73 @@ export default function AddEntryPage() {
   const { data: sessionData } = useSession();
 
   const cars = trpc.cars.getAll.useQuery();
+  const uploadImage = trpc.entries.uploadImage.useMutation();
   const addEntry = trpc.entries.insert.useMutation();
+
+  const [file, setFile] = useState<File | null>(null);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile: any = event.target.files ? event.target.files[0] : null;
+    setFile(selectedFile);
+  };
 
   async function handleAddEntryForm(e: any) {
     e.preventDefault();
-    const entry = {
-      track: {
-        connect: {
-          id: id,
-        },
-      },
-      car: {
-        connect: {
-          id: Number(e.target.car.value),
-        },
-      },
-      user: {
-        connect: {
-          id: sessionData?.user?.id,
-        },
-      },
 
-      performancePoints: Number(e.target.performancePoints.value),
-      drivetrain: String(e.target.drivetrain.value),
-      buildType: String(e.target.buildType.value),
-      time:
-        Number(e.target.minutes.value * 60) +
-        Number(e.target.seconds.value) +
-        Number(e.target.milliseconds.value / 1000),
-      shareCode: String(e.target.shareCode.value),
-    };
+    if (!file) {
+      console.log("Please provide a file");
+      return;
+    }
 
-    await addEntry.mutateAsync(EntryCreateOneSchema.parse({ data: entry }));
-    router.push("/" + id);
+    let gcsConnection: any = await uploadImage.mutateAsync(file.name);
+
+    console.log(gcsConnection);
+
+    const uploadResponse = await fetch(gcsConnection.signedUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "image/jpeg", //file.type,
+      },
+      body: file,
+    });
+
+    if (uploadResponse.ok) {
+      console.log("Image uploaded successfully!");
+      console.log(gcsConnection.publicUrl);
+
+      const entry = {
+        timestamp: new Date(),
+        track: {
+          connect: {
+            id: id,
+          },
+        },
+        car: {
+          connect: {
+            id: Number(e.target.car.value),
+          },
+        },
+        user: {
+          connect: {
+            id: sessionData?.user?.id,
+          },
+        },
+
+        performancePoints: Number(e.target.performancePoints.value),
+        drivetrain: String(e.target.drivetrain.value),
+        buildType: String(e.target.buildType.value),
+        time:
+          Number(e.target.minutes.value * 60) +
+          Number(e.target.seconds.value) +
+          Number(e.target.milliseconds.value / 1000),
+        shareCode: String(e.target.shareCode.value),
+        screenshotUrl: gcsConnection.publicUrl,
+      };
+
+      await addEntry.mutateAsync(EntryCreateOneSchema.parse({ data: entry }));
+      router.push("/" + id);
+    } else {
+      console.error("Image upload failed:", await uploadResponse.text());
+    }
   }
 
   if (!cars.data) {
@@ -73,14 +107,14 @@ export default function AddEntryPage() {
         <form onSubmit={handleAddEntryForm} className="flex flex-col">
           <div className="dark:text-white">Car</div>
           <Select name="car" options={carList} />
-          <div className="dark:text-white">Car Performance Points</div>
+          <div className="pt-3 dark:text-white">Car Performance Points</div>
           <input
             type="number"
             id="performancePoints"
             name="performancePoints"
             className="rounded bg-zinc-900 dark:text-white"
           />
-          <div className="grid grid-cols-2 gap-1">
+          <div className="grid grid-cols-2 gap-1 pt-3">
             <div className="w-full">
               <div className="text-center dark:text-white">Drivetrain</div>
               <select name="drivetrain" className="w-full">
@@ -102,7 +136,7 @@ export default function AddEntryPage() {
             </div>
           </div>
 
-          <div className="grid grid-cols-3 gap-1">
+          <div className="grid grid-cols-3 gap-1 pt-3">
             <div className="w-full">
               <div className="text-center dark:text-white">Minutes</div>
               <input
@@ -132,12 +166,22 @@ export default function AddEntryPage() {
             </div>
           </div>
 
-          <div className="dark:text-white">Sharecode (optional)</div>
+          <div className="pt-3 dark:text-white">Sharecode (optional)</div>
           <input
             type="text"
             id="shareCode"
             name="shareCode"
             className="rounded bg-zinc-900 dark:text-white"
+          />
+          <div className="pt-3 dark:text-white">
+            Provide a screenshot of the event end screen:
+          </div>
+          <input
+            id="file-input"
+            type="file"
+            accept="image/jpeg"
+            className="rounded border border-gray-500 bg-zinc-900 dark:text-white"
+            onChange={handleFileChange}
           />
           <div className="pt-5">
             {CardComponent(
